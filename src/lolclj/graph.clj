6a092218f1a69ee0b641bd66d0920d07ff6a61f6
David Creemer <david@zachary.com>
2012-01-02 02:14:01 -0800

    remove nodes and edges def from graph as it is redundant

--- src/lolclj/graph.clj
+++ src/lolclj/graph.clj
@@ -5,16 +5,6 @@
 
 ; Chapter 7, graph generation
 
-(def nodes
-  {:living-room "You are in the living-room. A wizard is snoring loudly on the couch."
-   :garden      "You are in a beautiful garden. There is a well in front of you."
-   :attic       "You are in the attic. There is a giant welding torch in the corner."})
-
-(def edges
-  {:living-room [[:garden :west :door], [:attic :upstairs :ladder]]
-   :garden      [[:living-room :east :door]]
-   :attic       [[:living-room :downstairs :ladder]]})
-
 (defn substitute-if [val pred coll]
   (for [i coll] (if (pred i) val i)))
 
